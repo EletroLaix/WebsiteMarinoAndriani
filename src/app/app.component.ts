79b6505748ef5688c2734db0c3f74a469ea42093
Marino Andriani <m.andriani@roboze.com>
2023-04-01 08:31:39 +0200

    Ripulito codice di prova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SiteutilityService } from './service/siteutility.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {}
+export class AppComponent {
+	constructor(public SiteUtility: SiteutilityService) {}
+}
--- src/app/service/personaldata.service.ts
+++ src/app/service/personaldata.service.ts
@@ -7,7 +7,6 @@ export class PersonaldataService {
 	FirstName = 'Marino';
 	Surname = 'Andriani';
 	FullName = this.FirstName + ' ' + this.Surname;
-	MotivationalPhrase =
-		"La perfezione non esiste ma esiste una tolleranza abbastanza piccola che da l'illusione della sua esistenza";
+	MotivationalPhrase = "Appassionato programmazione";
 	constructor() {}
 }
