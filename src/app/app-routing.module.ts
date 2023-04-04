00fa1222daf9700a0296f2b399860006ff02b805
Marino Andriani <m.andriani@roboze.com>
2023-04-04 19:55:18 +0200

    Introdotti nuovi content: - Work - Contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './component/about/about.component';
 import { EmptyComponent } from './component/empty/empty.component';
+import { WorkComponent } from './component/work/work.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 const routes: Routes = [
 	{ path: '', component: EmptyComponent },
 	{ path: 'About', component: AboutComponent },
+	{ path: 'Work', component: WorkComponent },
+	{ path: 'Contact', component: ContactComponent },
 ];
 
 @NgModule({
