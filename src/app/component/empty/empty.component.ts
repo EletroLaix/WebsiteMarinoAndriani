0331061950a1cc8d852579e1414c00f4bc0ee0f2
Marino Andriani <marino-andriani@live.it>
2026-08-08 22:31:40 +0200

    deploy: update GitHub Pages with redesigned site

--- src/app/component/empty/empty.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-empty',
-    templateUrl: './empty.component.html',
-    styleUrls: ['./empty.component.sass'],
-    standalone: false
-})
-export class EmptyComponent {}
